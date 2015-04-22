48389309048048423a4efac1022e4dc3ea092d86
teohrt <teohrt@iastate.edu>
2015-04-22 00:26:27 -0500

    buggy splay implentation

--- Splay Trees and Maps/SplayTreeSet.java
+++ Splay Trees and Maps/SplayTreeSet.java
@@ -15,244 +15,332 @@ public class SplayTreeSet<E extends Comparable<? super E>> extends AbstractSet<E
 {
 	//for toString
 	private StringBuilder sb;
-  // The root of the tree containing this set's items	
-  Node<E> root;
-	
-  // The total number of elements stored in this set
-  int size;
-  
-  /**
-   * Default constructor.  Creates a new, empty, SplayTreeSet
-   */
-  public SplayTreeSet() {
-	// TODO  
-  }
-  
-  /**
-   * Shallow copy constructor.  This method is fully implemented and should not be
-   * modified.
-   */
-  public SplayTreeSet(Node<E> root, int size) {
-	this.root = root;
-	this.size = size;
-  }
+	// The root of the tree containing this set's items	
+	Node<E> root;
 
-  /**
-   * Gets the root of this tree.  Used only for testing. This method is fully implemented
-   * and should not be modified.
-   * @return the root of this tree.
-   */
-  public Node<E> getRoot() 
-  {
-    return root; 
-  }
+	// The total number of elements stored in this set
+	int size;
 
-  /**
-   * Determines whether the set contains an element.  Splays at the node that stores the 
-   * element.  If the element is not found, splays at the last node on the search path.
-   * @param  obj  element to be determined whether to exist in the tree
-   * @return true if the element is contained in the tree and false otherwise
-   */
-  @Override
-  public boolean contains(Object obj)
-  {
-	  // TODO
-	  return true; 
-  }
+	/**
+	 * Default constructor.  Creates a new, empty, SplayTreeSet
+	 */
+	public SplayTreeSet() {
+		this(null, 0);
+	}
 
-  /**
-   * Inserts an element into the splay tree. In case the element was not contained, this  
-   * creates a new node and splays the tree at the new node. If the element exists in the 
-   * tree already, it splays at the node containing the element. 
-   * @param  key  element to be inserted
-   * @return true if insertion is successful and false otherwise
-   */
-  @Override
-  public boolean add(E key)
-  {
-	  // TODO
-	  return true; 
-  }
+	/**
+	 * Shallow copy constructor.  This method is fully implemented and should not be
+	 * modified.
+	 */
+	public SplayTreeSet(Node<E> root, int size) {
+		this.root = root;
+		this.size = size;
+	}
 
-  /**
-   * Removes the node that stores an element.  Splays at its parent node after removal
-   * (No splay if the removed node was the root.) If the node was not found, the last node 
-   * encountered on the search path is splayed to the root.
-   * @param obj  element to be removed from the tree
-   * @return true if the object is removed and false if it was not contained in the tree. 
-   */  
-  @Override
-  public boolean remove(Object obj)
-  {
-	  // TODO
-	  return true; 
-  }
-  
-  /**
-   * Returns the node containing key, or null if the key is not
-   * found in the tree.  Called by contains().
-   * @param key
-   * @return the node containing key, or null if not found
-   */
-  protected Node<E> findEntry(E key)
-  {
-    // TODO
-    return null; 
-  }
-  
-  
-  /**
-   * Returns the successor of the given node.
-   * @param n
-   * @return the successor of the given node in this tree, 
-   *   or null if there is no successor
-   */
-  protected Node<E> successor(Node<E> n)
-  {
-	  // TODO
-	  return null; 
-  }
-  
-  /**
-   * Removes the given node, preserving the binary search
-   * tree property of the tree.
-   * @param n node to be removed
-   */
-  protected void unlinkNode(Node<E> n)
-  {
-	  // TODO
-  }
-  
-  @Override
-  public Iterator<E> iterator()
-  {
-    return new SplayTreeIterator();
-  }
+	/**
+	 * Gets the root of this tree.  Used only for testing. This method is fully implemented
+	 * and should not be modified.
+	 * @return the root of this tree.
+	 */
+	public Node<E> getRoot() 
+	{
+		return root; 
+	}
 
-  @Override
-  public int size()
-  {
-	  // TODO
-	  return 0; 
-  }
- 
-  /**
-   * Returns a representation of this tree as a multi-line string as
-   * explained in the project description.
-   */
-  @Override
-  public String toString()
-  {
-	  sb = new StringBuilder();
-	  toStringHelper(sb, root, 0);
-	  return sb.toString(); 
-  }
-  /**
-   * to string recursive helper method
-   */
-  private void toStringHelper(StringBuilder sb, Node<E> n, int d){
-	  spaceHelper(d); //spaces
-	 
-	  if (n.getLeft() ==null && n.getRight()==null){ // if leaf
-		  sb.append(n.toString() + "\n");
-		  return;
-	  }
-	   sb.append(n.toString() + "\n");
-	  
-	  if(n.getLeft()!=null){
-	  toStringHelper(sb, n.getLeft(), d+1);
-	  }
-	  if(n.getLeft()==null){
-		  spaceHelper(d+1);
-		  sb.append("null\n");
-	  }
-	  if (n.getRight()!=null){
-	  toStringHelper(sb, n.getRight(), d+1);
-	  }
-	  if(n.getRight()==null){
-		  spaceHelper(d+1);
-		  sb.append("null\n");
-	  }
-	  return;
-  }
-  /**
-   * toString helperhelper. spaces man. s p a c e s 
-   */
-  private void spaceHelper(int d){
-	  for(int i=0; i< (4 * d); i++){
-		  sb.append(" ");
-	  }
-  }
-  /**
-   * Splay at the current node.  This consists of a sequence of zig, zigZig, or zigZag 
-   * operations until the current node is moved to the root of the tree.
-   * @param current  node at which to splay.
-   */
-  protected void splay(Node<E> current)
-  {
-    // TODO
-  }	
+	/**
+	 * Determines whether the set contains an element.  Splays at the node that stores the 
+	 * element.  If the element is not found, splays at the last node on the search path.
+	 * @param  obj  element to be determined whether to exist in the tree
+	 * @return true if the element is contained in the tree and false otherwise
+	 */
+	@Override
+	public boolean contains(Object obj)
+	{	  
+		Node<E> cur = root;
+		while(cur != null){
+			if (cur.getData().compareTo((E) obj)<0){		//if cur is bigger than object
+				cur=cur.getRight();
+			}
+			else if(cur.getData().compareTo((E) obj)>0){	//if cur is less than object
+				cur=cur.getLeft();
+			}
+			else
+				return true;
+		}
+		return false;
+	}
 
-  /**
-   * Performs the zig operation on a node.
-   * @param current  node at which to perform the zig operation.
-   */
-  protected void zig(Node<E> current)
-  {
-    // TODO
-  }
+	/**
+	 * Inserts an element into the splay tree. In case the element was not contained, this  
+	 * creates a new node and splays the tree at the new node. If the element exists in the 
+	 * tree already, it splays at the node containing the element. 
+	 * @param  key  element to be inserted
+	 * @return true if insertion is successful and false otherwise
+	 */
+	@Override
+	public boolean add(E key)
+	{
+		// TODO
+		return true; 
+	}
 
-  /**
-   * Performs the zig-zig operation on a node.
-   * @param current  node at which to perform the zig-zig operation.
-   */
-  protected void zigZig(Node<E> current)
-  {
-    // TODO
-  }
+	/**
+	 * Removes the node that stores an element.  Splays at its parent node after removal
+	 * (No splay if the removed node was the root.) If the node was not found, the last node 
+	 * encountered on the search path is splayed to the root.
+	 * @param obj  element to be removed from the tree
+	 * @return true if the object is removed and false if it was not contained in the tree. 
+	 */  
+	@Override
+	public boolean remove(Object obj)
+	{
+		// TODO
+		return true; 
+	}
 
-  /**
-   * Performs the zig-zag operation on a node.
-   * @param current  node to perform the zig-zag operation on
-   */
-  protected void zigZag(Node<E> current)
-  {
-    // TODO
-  }    
-  
-  /**
-   *
-   * Iterator implementation for this splay tree.  The elements are
-   * returned in ascending order according to their natural ordering.
-   *
-   */
-  private class SplayTreeIterator implements Iterator<E>
-  {
-    Node<E> cursor;
+	/**
+	 * Returns the node containing key, or null if the key is not
+	 * found in the tree.  Called by contains().
+	 * @param key
+	 * @return the node containing key, or null if not found
+	 */
+	protected Node<E> findEntry(E key)
+	{
+		// TODO
+		return null; 
+	}
 
-    public SplayTreeIterator()
-    {
-    	// TODO
-    }
-    
-    @Override
-    public boolean hasNext()
-    {
-    	// TODO
-    	return true; 
-    }
 
-    @Override
-    public E next()
-    {
-    	// TODO
-    	return null; 
-    }
+	/**
+	 * Returns the successor of the given node.
+	 * @param n
+	 * @return the successor of the given node in this tree, 
+	 *   or null if there is no successor
+	 */
+	protected Node<E> successor(Node<E> n)
+	{
+		return n.getParent(); 
+	}
 
-    @Override
-    public void remove()
-    {
-      // TODO
-    }
-  }
+	/**
+	 * Removes the given node, preserving the binary search
+	 * tree property of the tree.
+	 * @param n node to be removed
+	 */
+	protected void unlinkNode(Node<E> n)
+	{
+		// TODO
+	}
+
+	@Override
+	public Iterator<E> iterator()
+	{
+		return new SplayTreeIterator();
+	}
+
+	@Override
+	public int size()
+	{
+		return size; 
+	}
+
+	/**
+	 * Returns a representation of this tree as a multi-line string as
+	 * explained in the project description.
+	 */
+	@Override
+	public String toString()
+	{
+		sb = new StringBuilder();
+		toStringHelper(sb, root, 0);
+		return sb.toString(); 
+	}
+	/**
+	 * to string recursive helper method
+	 */
+	private void toStringHelper(StringBuilder sb, Node<E> n, int d){
+		spaceHelper(d); //spaces
+
+		if (n.getLeft() ==null && n.getRight()==null){ // if leaf
+			sb.append(n.toString() + "\n");
+			return;
+		}
+		sb.append(n.toString() + "\n");
+
+		if(n.getLeft()!=null){
+			toStringHelper(sb, n.getLeft(), d+1);
+		}
+		if(n.getLeft()==null){
+			spaceHelper(d+1);
+			sb.append("null\n");
+		}
+		if (n.getRight()!=null){
+			toStringHelper(sb, n.getRight(), d+1);
+		}
+		if(n.getRight()==null){
+			spaceHelper(d+1);
+			sb.append("null\n");
+		}
+		return;
+	}
+	/**
+	 * toString helperhelper. spaces man. s p a c e s 
+	 */
+	private void spaceHelper(int d){
+		for(int i=0; i< (4 * d); i++){
+			sb.append(" ");
+		}
+	}
+
+	/**
+	 * rotation. left child to parent
+	 */
+	private void leftToParent(Node<E> child, Node<E> parent){
+		if ((child == null) || (parent==null) || (parent.getLeft() != child)) throw new RuntimeException();
+
+		if(parent.getParent() != null){
+			if (parent == parent.getParent().getLeft()){
+				parent.getParent().setLeft(child);
+			}
+			else{
+				parent.getParent().setRight(child);
+			}
+			if(child.getRight()!= null){
+				child.getRight().setParent(parent);
+			}
+			child.setParent(parent.getParent());
+			parent.setParent(child);
+			parent.setLeft(child.getRight());
+			child.setRight(parent);
+		}
+	}
+	/**
+	 * rotation. right child to parent
+	 */
+	private void rightToParent(Node<E> child, Node<E> parent){
+		if ((child == null) || (parent==null) || (parent.getLeft() != child)) throw new RuntimeException();
+
+		if(parent.getParent() != null){
+			if (parent == parent.getParent().getLeft()){
+				parent.getParent().setLeft(child);
+			}
+			else{
+				parent.getParent().setRight(child);
+			}
+			if(child.getLeft()!= null){
+				child.getLeft().setParent(parent);
+			}
+			child.setParent(parent.getParent());
+			parent.setParent(child);
+			parent.setRight(child.getLeft());
+			child.setLeft(parent);
+		}
+	}
+	/**
+	 * Splay at the current node.  This consists of a sequence of zig, zigZig, or zigZag 
+	 * operations until the current node is moved to the root of the tree.
+	 * @param current  node at which to splay.
+	 */
+	protected void splay(Node<E> current)
+	{
+		// TODO
+		while(current.getParent() != null){
+			Node<E> Parent = current.getParent();
+			Node<E> GrandParent = Parent.getParent();
+			if (GrandParent == null){
+				if (current == Parent.getLeft()){
+					leftToParent(current, Parent);
+				}
+				else{
+					rightToParent(current, Parent);
+				}
+			}
+			else{
+				if(current==Parent.getLeft()){
+					if (Parent == GrandParent.getLeft()){
+						leftToParent(Parent, GrandParent);
+						leftToParent(current, Parent);
+					}
+					else{
+						leftToParent(current, current.getParent());
+						rightToParent(current, current.getParent());
+					}
+				}
+				else{
+					if(Parent==GrandParent.getLeft()){
+						rightToParent(current, current.getParent());
+						leftToParent(current, current.getParent());
+					}
+					else{
+						rightToParent(Parent, GrandParent);
+						rightToParent(current, Parent);
+					}
+				}
+			}
+		}
+		root=current;
+	}	
+
+	/**
+	 * Performs the zig operation on a node.
+	 * @param current  node at which to perform the zig operation.
+	 */
+	protected void zig(Node<E> current)
+	{
+		// TODO
+	}
+
+	/**
+	 * Performs the zig-zig operation on a node.
+	 * @param current  node at which to perform the zig-zig operation.
+	 */
+	protected void zigZig(Node<E> current)
+	{
+		// TODO
+	}
+
+	/**
+	 * Performs the zig-zag operation on a node.
+	 * @param current  node to perform the zig-zag operation on
+	 */
+	protected void zigZag(Node<E> current)
+	{
+		// TODO
+	}    
+
+	/**
+	 *
+	 * Iterator implementation for this splay tree.  The elements are
+	 * returned in ascending order according to their natural ordering.
+	 *
+	 */
+	private class SplayTreeIterator implements Iterator<E>
+	{
+		Node<E> cursor;
+
+		public SplayTreeIterator()
+		{
+			// TODO
+		}
+
+		@Override
+		public boolean hasNext()
+		{
+			// TODO
+			return true; 
+		}
+
+		@Override
+		public E next()
+		{
+			// TODO
+			return null; 
+		}
+
+		@Override
+		public void remove()
+		{
+			// TODO
+		}
+	}
 }
